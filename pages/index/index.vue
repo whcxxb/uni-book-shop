2fdbfb84a5736f0f316b62e9c6e2494e04ef7c55
whcxxb <731896502@qq.com>
2022-01-14 01:11:36 +0800

    up

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,8 +11,20 @@
 				title: 'Hello'
 			}
 		},
-		onLoad() {
-
+	async	onLoad() {
+		 // const data = await this.$u.get('/api/index')
+		 // console.log(data)
+		 // console.log(2222)
+		 
+		 // const data = await this.$u.get('/api/orders/1/confirm',{name:'liming'})
+		 // const res = await this.$u.api.index()
+		 // console.log(res)
+		 const data = {
+			 email:'test@a.com',
+			 password:123123
+		 }
+		 const res = await this.$u.api.authLogin()
+		 console.log(res)
 		},
 		methods: {
 
